453bbe0c3e03eb108eab5e14f024920bf0ff987d
Ushie <ushiekane@gmail.com>
2023-10-22 20:55:25 +0300

    fix: non-animated emojis aren't displayed

--- src/lib/utils/customEmoteToUrl.ts
+++ src/lib/utils/customEmoteToUrl.ts
@@ -1,8 +1,8 @@
 function customEmoteToUrl(emote: string) {
-    return (emote = emote.replace(
-        /<a:(.*?):(\d+)>/g,
-        "![$1](https://cdn.discordapp.com/emojis/$2.gif)"
-    ));
+    return (emote = emote.replace(/<a?:(.*?):(\d+)>/g, (match, p1, p2) => {
+        const extension = match.startsWith("<a:") ? "gif" : "png";
+        return `![${p1}](https://cdn.discordapp.com/emojis/${p2}.${extension})`;
+    }));
 }
 
 export default customEmoteToUrl;
